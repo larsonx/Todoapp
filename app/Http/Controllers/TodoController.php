455628b01a7c9a477d1ee6c2ebab87499839f7b8
larson cop <97100495@st.deltion.nl>
2023-12-02 18:01:43 +0100

    changes

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
 use App\Models\Todo;
+use Illuminate\Http\Request;
+
 
 
 class TodoController extends Controller
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::resource('todos',TodoController::class);
 
 
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
