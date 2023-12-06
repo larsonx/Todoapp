7eb12af56a0c0a69696b7bfb5f18a6a5281482f1
larson cop <97100495@st.deltion.nl>
2023-12-06 15:47:50 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,15 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard/create', [TodoController::class, 'create'])->name('dashboard.create');
     Route::post('/dashboard', [TodoController::class, 'store'])->name('dashboard.store');
     Route::get('/dashboard.edit/{todo}/edit', [TodoController::class, 'edit'])->name('dashboard.edit');
+    Route::get('/dashboard.show/{todo}', [TodoController::class, 'show'])->name('dashboard.show');
     Route::delete('/dashboard/{todo}', [TodoController::class, 'destroy'])->name('dashboard.destroy');
+
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/dashboard.show/{todo}', [TodoController::class, 'show'])->name('dashboard.show'); {
-        return redirect('todos.display');
-    }
+  
 });
 
 require __DIR__.'/auth.php';    
