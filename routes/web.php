8a371d0eb21dde29492051813e0723985230998c
larson cop <97100495@st.deltion.nl>
2023-12-06 15:42:07 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\RedirectResponse;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,13 +13,16 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [TodoController::class, 'index'])->name('dashboard');
     Route::get('/dashboard/create', [TodoController::class, 'create'])->name('dashboard.create');
     Route::post('/dashboard', [TodoController::class, 'store'])->name('dashboard.store');
-    Route::get('/todos. show/{todo}', [TodoController::class, 'show'])->name('dashboard.show');
     Route::get('/dashboard.edit/{todo}/edit', [TodoController::class, 'edit'])->name('dashboard.edit');
     Route::delete('/dashboard/{todo}', [TodoController::class, 'destroy'])->name('dashboard.destroy');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard.show/{todo}', [TodoController::class, 'show'])->name('dashboard.show'); {
+        return redirect('todos.display');
+    }
 });
 
 require __DIR__.'/auth.php';    
